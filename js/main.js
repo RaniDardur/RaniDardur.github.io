3e9db2b4f7e013f7e579f75d1d593ddf857366b7
HunterHF <53719535+HunterHF@users.noreply.github.com>
2019-11-04 11:17:03 +0300

    321\

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-// Отправка заявки 
 $(document).ready(function() {
-	$('#form').submit(function() { // проверка на пустоту заполненных полей. Атрибут html5 — required не подходит (не поддерживается Safari)
+	$('#form').submit(function() {
 		if (document.form.name.value == '' || document.form.user_email.value == '' ) {
 			valid = false;
 			return valid;
@@ -19,3 +18,42 @@ $(document).ready(function() {
 	});
 });
 
+$(document).ready(function() {
+	$('#form1').submit(function() {
+		if (document.form1.name1.value == '' || document.form1.user_email1.value == '' ) {
+			valid = false;
+			return valid;
+		}
+		$.ajax({
+			type: "POST",
+			url: "mail1.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$('.fields1').addClass('fields_none');
+			$('.note1').fadeIn(1000);
+			$(this).find('input').val('');
+			$('#form1').trigger('reset');
+		});
+		return false;
+	});
+});
+
+$(document).ready(function() {
+	$('#form2').submit(function() {
+		if (document.form2.name2.value == '' || document.form2.user_email2.value == '' ) {
+			valid = false;
+			return valid;
+		}
+		$.ajax({
+			type: "POST",
+			url: "mail2.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$('.fields2').addClass('fields_none');
+			$('.note2').fadeIn(1000);
+			$(this).find('input').val('');
+			$('#form2').trigger('reset');
+		});
+		return false;
+	});
+});
