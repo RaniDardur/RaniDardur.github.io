54f42a616bd53c81e55c3bfa5dd16d8cdd3eb04f
HunterHF <53719535+HunterHF@users.noreply.github.com>
2019-11-17 03:49:06 +0300

    new

--- js/js.js
+++
@@ -1,8 +0,0 @@
-$('.block-price__btn').click(function(){
-	var price = $(this).data('price'),
-		product = $(this).data('product');
-	$('#price').val(price);
-	$('#product').val(product);
-	$('#myModal').modal();
-	return false;
-});
