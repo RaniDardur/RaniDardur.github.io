20481600bf83eb8e7dd07bce067169d59b04e54d
HunterHF <53719535+HunterHF@users.noreply.github.com>
2019-12-09 02:55:07 +0300

    3

--- mail.php
+++ mail.php
@@ -4,9 +4,8 @@ $recepient = "dardurranisc@gmail.com";
 $siteName = "Serre";
 
 $name = trim($_POST["name"]);
-$email = trim($_POST["user_email"]);
 $phone = trim($_POST["user_phone"]);
-$message = "Имя: $name \nПочта: $email\nТелефон: $phone ";
+$message = "Имя: $name \nТелефон: $phone ";
 
 $pagetitle = "Заявка с сайта \"$siteName\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
